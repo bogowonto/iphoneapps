515bf12fffb397d3b7de235fcf05f91bf7faad76
B. Yusri <byusri75@gmail.com>
2016-09-01 14:47:02 +0200

    adding mkdir
    change owner
    
    print path

--- git_command.py
+++ git_command.py
@@ -3,3 +3,8 @@ import os
 file_path = "/Users/yusri/Bogowonto/Workspace/Projects/github/iphoneapps/"
 
 os.chdir(file_path)
+os.mkdir("testfolder", 0o775)
+
+print(os.path)
+
+os.chmod("testfolder", 0o755)
